2fde94899e5777c971eb2a5045647e4e27a5d140
sandeep.jangra <sandeep.jangra@kony.com>
2017-11-08 12:25:54 +0530

    chaos file converted to .ejs

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/chaos',function(req,res)){
-	response.render('pages/chaos.html')
-}
+app.get('/chaos', function(request ,response ){
+	response.render('pages/chaos')
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
